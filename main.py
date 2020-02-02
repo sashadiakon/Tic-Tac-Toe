5e3f3bbe09a06e75027a5a28cc897361b5e78ef1
Sasha <sashadiakon4@gmail.com>
2020-02-03 01:25:46 +0200

    New design

--- main.py
+++ main.py
@@ -5,22 +5,28 @@ from kivy.uix.widget import Widget
 from kivy.graphics import (Color, Rectangle, Line)
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.screenmanager import ScreenManager , Screen
 from kivy.uix.popup import Popup
-from kivy.core.window import Window
-# from kivy.config import Config
+from kivy.uix.image import Image
+# from kivy.core.window import Window
+from kivy.config import Config
 import json
-from time import asctime
-height = Window.height
-width = Window.width
-# Config.set("graphics", "width", width)
-# Config.set("graphics", "height", height)
-# Config.set("graphics", "resizble", 0)
+from time import localtime
+# height = Window.height
+# width = Window.width
+height = 800
+width = 630
+Config.set("graphics", "width", width)
+Config.set("graphics", "height", height)
+Config.set("graphics", "resizble", 0)
 print(height)
 pad = 20
 spac = 20
-h_up = 0.15
+h_up = 0.1
+
 h_down = 0.2
 h_downp = h_down * height
 h_upp = h_up * height
@@ -30,7 +36,7 @@ class BackgroundWidget(Widget):
     def __init__(self, **kwargs):
         super(BackgroundWidget, self).__init__(**kwargs)
         with self.canvas:
-            Color(1, 1, 1, 1)
+            Color(.8, .7, 1, 1)
             Rectangle(pos = (0, 0), size = (width, height))
             Color(0, 0, 0, 1)
 
@@ -38,29 +44,35 @@ class BackgroundWidget(Widget):
             spaces_height = pad + int(spac / 2) + h_downp
             y = spaces_height + height_btn_field
             x = spaces_width + width_btn_field
-            Line(points=(x, h_downp + pad + 15, x, \
-                         height - h_upp - pad), width=spac / 2)
-            Line(points=(pad, y, width - pad, y), width=spac / 2)
+            Line(points = (x, h_downp + pad + 15, x, \
+                         height - h_upp - pad), width = spac / 2)
+            Line(points = (pad, y, width - pad, y), width = spac / 2)
 
             spaces_width = pad + spac * 1.5
             spaces_height = pad + spac * 1.5 + h_downp
             y = spaces_height + height_btn_field * 2
             x = spaces_width + width_btn_field * 2
-            Line(points=(x, h_downp + pad + 15, x, \
+            Line(points = (x, h_downp + pad + 15, x, \
                          height - h_upp - pad), width=spac / 2)
-            Line(points=(pad, y, width - pad, y), width=spac / 2)
-
-
+            Line(points = (pad, y, width - pad, y), width=spac / 2)
+class Back(Widget):
+    def __init__(self, **kwargs):
+        super(Back, self).__init__(**kwargs)
+        with self.canvas:
+            Color(.8, .7, 1, 1)
+            Rectangle(pos = (0,0), size = (width, height))
 class DownPanelWidget(BoxLayout):
     def __init__(self, **kwargs):
         super(DownPanelWidget, self).__init__(**kwargs)
         self.orientation = "horizontal"
-        btn_stat = Button(text = "Statistic", on_press = self.statistic)
-        # btn_type = Button(text = "Multiple", on_press = self.choose_type)
-        # btn_sett = Button(text =  "Settings", on_press = self.go_sett)
+        self.spacing = width/20
+        self.padding = width/20
+        btn_stat = Button(text = "Last Games",on_press = self.statistic,\
+font_size = width/30, background_color = (.5, .75, 1, 1))
+        btn_type = Button(text = "Multiple", on_press = self.choose_type,\
+font_size = width/30, background_color = (.5, .75, 1, 1))
         self.add_widget(btn_stat)
-        # self.add_widget(btn_sett)
-        # self.add_widget(btn_type)
+        self.add_widget(btn_type)
     def go_sett(self, instance):
         global sm
         sm.current = "settings"
@@ -70,41 +82,43 @@ class DownPanelWidget(BoxLayout):
     def choose_type(self, instance):
         pass
 
-class SettingsScreen(Screen, Widget):
-    def __init__(self, **kwargs):
-        super(SettingsScreen, self).__init__(**kwargs)
-
 class HistoryScreen(Screen, BoxLayout):
     def __init__(self, **kwargs):
         super(HistoryScreen, self).__init__(**kwargs)
-        bl = BoxLayout(orientation = "vertical")
+        bll = BoxLayout(orientation = "vertical")
+        bl = GridLayout(cols = 3)
         js = json.load(open("history.json"))
         print(js)
 
         tx = ""
-        n = 3
+        n = 9
         listt = []
         if len(js)>n:
             for i in range(0, n):
                 listt.append(i)
         else:
-            for i in range(0,len(js)):
+            for i in range(0, len(js)):
                 listt.append(i)
-        bl.add_widget(Button(text = "Back", on_press = self.go_main, \
-size_hint = (0.15, 0.1), font_size = 35))
+        bl1 = BoxLayout(size_hint = (1, 0.15), orientation = "horizontal")
+        bl1.add_widget(Button(text = "Back", on_press = self.go_main, \
+size_hint_x = 0.2, font_size = width*0.15/4))
+        bl1.add_widget(Label(text = "Last Games:", font_size = width/12))
+        bll.add_widget(bl1)
         for i in listt:
-            print(i)
+
             match = js[i]
             field = match[1]
+
             fl = [field[0][0] + "|" + field[0][1] + "|" + field[0][2], \
 field[1][0] + "|" + field[1][1] + "|" + field[1][2], \
 field[2][0] + "|" + field[2][1] + "|" + field[2][2]]
             tx =  "\n" + match[2] + "\n" + match[0] + \
 "\n     " + fl[0] + "\n     " + fl[1] + "\n     " + fl[2]
 
-            bl.add_widget(Label(text = tx, color = (1,1,1,1), halign = "left", font_size = 25))
-
-        self.add_widget(bl)
+            bl.add_widget(Label(text = tx, color = (0,0,0,1), halign = "left", font_size = width/24))
+        bll.add_widget(bl)
+        self.add_widget(Back())
+        self.add_widget(bll)
     def go_main(self, instance):
         global sm
         sm.current = "main"
@@ -114,7 +128,7 @@ class MainScreen(Screen, Widget):
         super(MainScreen, self).__init__(**kwargs)
         bl = BoxLayout(orientation = "vertical")
         self.grid = GridLayout(cols = 3, rows = 3, spacing = spac, padding = pad, size_hint = (1, 1-h_up-h_down))
-        self.btn = Button(text = "", background_color = (.9, .4, .4, 1), on_press = self.end)
+        self.btn = Button(text = "", background_color = (.4, .5, 1, 1), on_press = self.end, font_size = 0.4*width/16)
         self.step = "X"
         self.popup = Popup(title = "The end of game", content = self.btn , size_hint = (.4, .2))
         self.lbl_stat = Label(text = "Turn: X!", font_size = int(width/14), size_hint = (1, h_up), color = (0,0,0,1))
@@ -122,6 +136,7 @@ class MainScreen(Screen, Widget):
         self.win = False
         self.winner = ""
         self.listt = []
+        self.free_fields = 9
         self.free_fields = 9
         if width_btn_field >= height_btn_field:
             font = height_btn_field / 1.5
@@ -138,18 +153,22 @@ class MainScreen(Screen, Widget):
         self.add_widget(BackgroundWidget())
         self.add_widget(bl)
     def presss(self, instance):
-        print(instance.text)
-
         if instance.text == "":#Validation of clear
-            self.free_fields -= 1
             # Next figure
-            instance.text = self.step
-            if self.step == "X":
-                instance.color = (.25, .6, 1, 1)
-            else:
-                instance.color = (0,0,0,1)
-
+            # instance.text = self.step
             n = self.listt.index(instance)
+            if instance.text == "":  # Validation of clear
+                self.free_fields -= 1
+                # Next figure
+                instance.text = self.step
+                if self.step == "X":
+                    instance.color = (.25, .6, 1, 1)
+                else:
+                    instance.color = (0, 0, 0, 1)
+
+                n = self.listt.index(instance)
+
+
 
 
             '''This will insert into list.'''
@@ -187,10 +206,13 @@ class MainScreen(Screen, Widget):
             if self.win:
                 self.btn.text = "The winner is " + winner
                 history = json.load(open("history.json"))
-                tm = asctime()
-                while len(history)>= 10:
+                tm = list(map(str, localtime()))
+                while len(history)>= 12:
                     history.pop(len(history)-1)
-                history.append(["Winner: " + winner, self.field, tm])
+                for i in range(1, 5):
+                    if int(tm[i]) < 10:
+                        tm[i] = "0" + tm[i]
+                history.insert(0, ["Winner: " + winner, self.field, tm[1]+"."+tm[2]+", "+tm[3]+":"+tm[4]])
                 with open("history.json", "w") as file:
                     file.write(json.dumps(history))
                 self.popup.open()
@@ -202,9 +224,15 @@ class MainScreen(Screen, Widget):
                 self.free_fields = 9
 
             elif self.free_fields == 0:#Draw
+                tm = list(map(str, localtime()))
                 self.btn.text = "Draw"
                 history = json.load(open("history.json"))
-                history.append(["Draw", self.field])
+                while len(history)>= 12:
+                    history.pop(len(history)-1)
+                for i in range(1,5):
+                    if int(tm[i]) < 10:
+                        tm[i] = "0"+tm[i]
+                history.insert(0, ["Draw", self.field, tm[1]+"."+tm[2]+", "+tm[3]+":"+tm[4]])
                 with open("history.json", "w") as file:
                     file.write(json.dumps(history))
                 self.popup.open()
@@ -226,7 +254,6 @@ class MainScreen(Screen, Widget):
 sm = ScreenManager()
 sm.add_widget(MainScreen(name = "main"))
 sm.add_widget(HistoryScreen(name = "history"))
-sm.add_widget(SettingsScreen(name = "settings"))
 sm.current = "main"
 class My_App(App):
     def build(self):
